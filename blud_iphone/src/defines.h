a349d717499eb28559f3f6e19b21c1a8780cbf78
jonbro <jon@walltowall.com>
2011-01-20 20:23:15 -0500

    removed all the early structure

--- blud_iphone/src/defines.h
+++
@@ -1,1 +0,0 @@
-#define method(class, name) {#name, &class::name}
--- blud_osx/bin/data/test_scripts/include_test/include_test.lua
+++
@@ -1,9 +0,0 @@
-dofile(blud.bundle_root .. "/test_scripts/include_test/class.lua")
-dofile(blud.bundle_root .. "/test_scripts/include_test/Rectangle.lua")
-
-test_rect = Rectangle(100,100);
-test_rect:setPosition(100,100);
-
-function blud.draw()
-  test_rect:draw()
-end
