13077d15fb926e396f50a205eddcd0868d79b5fe
jonbro <jonbro@gmail.com>
2011-03-25 16:47:52 -0400

    changed the spritesheet to make it actually work

--- examples/bludSpriteSheet.lua
+++ examples/bludSpriteSheet.lua
@@ -21,16 +21,17 @@ function blud.update(t)
     sheet:update(t)
     
     -- sort the table so that the lower sprites are drawn last
-    table.sort(sprites, function(a, b) return a.y<b.y end)
+    table.sort(sprites, function(a, b) return a.y>b.y end)
     
     -- loop through the sprite table
-    for i, v in ipairs(sprites) do
+    for i=table.maxn(sprites),1,-1 do
+        v = sprites[i]
         -- update the vertical position of the sprite
         v.y = v.y+v.speed
         
-        if(v.y > bg:getHeight+16)
+        if (v.y > bg:getHeight()+16) then
             -- if the sprite is off the screen, remove it from the array
-            v = nil
+            table.remove(sprites,i)
         else
             -- otherwise, add it to the sheet that is about to be drawn
             sheet:addCenteredTile(v.sprite, v.x, v.y)
@@ -38,14 +39,14 @@ function blud.update(t)
     end
     
     -- every frame add a whole bunch of sprites so that the screen fills with an army
-    for i=1,10 do
+    for i=1,2 do
         
         -- blud sprite is a container for the animation info that the spritesheet needs to render. It doesn't contain information about where to render on the screen though, so we need to store some additional information in the table
         sprite = bludSprite();
                 
         -- the animation length in frames
         sprite:setTotalFrames(4)
-        
+
         -- the number of tiles each animation frame is in width and height
         sprite:setWidth(1)
         sprite:setHeight(1)
@@ -53,17 +54,24 @@ function blud.update(t)
         -- how long each frame displays for
         sprite:setFrameDuration(75)
         
-        -- how many times the animation plays
+        -- change the start index of our sprite. we have 4 rows of animations and our spritesheet is 8 tiles wide, so our possible start indicies are 0, 8, 16, and 24
+        sprite:setIndex(math.floor(math.random(0,4))*8)
+        
+		-- how many times the animation plays
         sprite:setLoops(-1)
                 
         -- start the sprite at a random x position, and somewhere off the screen
-        sprites.insert({sprite=sprite, x=math.random(0, bg:getWidth()),y=-16, speed=math.random(1,5)});
+        table.insert(sprites, {sprite=sprite, x=math.random(0, bg:getWidth()),y=-16, speed=math.random(2,4)});
         
     end
-    sheet:addSprite(sprites)
 end
 
 function blud.draw()
     -- draws all the sprites that have been added to the sheet during the update function
     sheet:draw()
+end
+
+function blud.touch.down(x,y,id)
+    -- print out the size of the sprite array, for debugging purposes
+    print("num sprites:" .. table.maxn(sprites))
 end
