7f95104f5ec283037d36a8e68534ee95aeed0dea
jonbro <jonbro@gmail.com>
2011-07-15 12:59:16 +0100

    added line wrapping for blud font rendering

--- src/bludFont.h
+++ src/bludFont.h
@@ -19,7 +19,50 @@ public:
 		return 1;
 	}
 	int draw (lua_State *L) {
-		font.drawString(luaL_checkstring(L, 1), luaL_checknumber(L, 2), luaL_checknumber(L, 3));
+		// check to see if the text to draw includes a width...
+		// this is for building text boxes and auto wrapping text (this is the dumbest way to do this ever)
+		float width = -1;
+		if(lua_isnumber(L, 4)){
+			width = lua_tonumber(L, 4);
+		}
+		
+		string textString = luaL_checkstring(L, 1);
+		float x = luaL_checknumber(L, 2);
+		float y = luaL_checknumber(L, 3);
+		int line_height = 20;
+		
+		if(width > 0){
+			size_t start_pos = 0;
+			size_t end_pos = 0;
+			size_t test_end_pos = 0;
+			string line_test = "";
+			
+			while (end_pos < textString.length()) {
+				// move the test end pos forward
+				test_end_pos = textString.find_first_of(" ", test_end_pos+1);
+				line_test = textString.substr(start_pos, test_end_pos-start_pos);
+				// if we are under the width
+				if(font.stringWidth(line_test)<width){
+					// commit the end position and loop it again
+					end_pos = test_end_pos;
+				}else {
+					
+					// if we are over the width
+					// then print the line to the screen
+					font.drawString(textString.substr(start_pos, end_pos-start_pos), x, y+line_height);
+
+					// move the start position forward, and unwind the end test
+					end_pos++;
+					start_pos = end_pos;
+					test_end_pos--;
+					// increase the line height
+					line_height += 20;
+				}
+
+			}			
+		}else {
+			font.drawString(textString, x, y);
+		}
 		return 1;
 	}
 	int getWidth(lua_State *L){
