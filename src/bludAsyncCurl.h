138a15fdec41677ef7aecdda1d6e5b4cb86e2e99
jonbro <jonbro@gmail.com>
2011-11-10 14:49:59 +0000

    fixed up the async stuff

--- src/bludAsyncCurl.h
+++ src/bludAsyncCurl.h
@@ -53,28 +53,57 @@ public:
         if (error) {
             printf("%s\n", lua_tostring(luaVM, -1));
         }	
-
+        hasCallback = false;
         complete = false;
+        mutex = bludLock::getInstance();
 	}
     int process(lua_State *L) {
         // gets a chunk of lua code and processes it on another thread
         execute = luaL_checkstring(L, 1);
+        // check to see if there is an included callback function
+        if(lua_isfunction(L, 2)){
+            // push the value of the function (that is at position 1), onto the top of the stack
+            lua_pushvalue(L, 2);
+            // store this stack position in the registry index
+            callbackFunc = luaL_ref(L, LUA_REGISTRYINDEX);
+            hasCallback = true;
+            mainVM = L;
+        }
         startThread(true, false);   // blocking, verbose
         return 1;
     }
     //--------------------------
     void threadedFunction(){
         // do the actual processing of the lua code over here
+        int startStack = lua_gettop(luaVM);
         if(luaL_dostring(luaVM, execute.c_str()) != 0)
             printf("error in thread: %s\n", lua_tostring(luaVM, -1));
-
         // The function above pushes return values to the stack. We should probably store these and return them to the original thread
         if(lock()){
+            if(hasCallback){
+                // todo: should probably lock lua, so that things don't blow up here
+                mutex->lock();
+                numReturns = lua_gettop(luaVM) - startStack;
+                cout << "number of returns: " << numReturns << endl;
+                lua_rawgeti(mainVM, LUA_REGISTRYINDEX, callbackFunc );
+                lua_pushstring(mainVM, luaL_checkstring(luaVM, -1*numReturns));                
+                if(lua_pcall(mainVM, numReturns, 0, 0) != 0){
+                    ofLog(OF_LOG_ERROR, "async callback error");
+                    ofLog(OF_LOG_ERROR, lua_tostring(mainVM, -1));
+                }
+                mutex->unlock();
+            }
             complete = true;
             unlock();
         }else{
             
         }
+    };
+    int getReturnValue(lua_State *L) {
+        if(complete){
+            lua_pushstring(L, lua_tostring(luaVM,-1));
+        }
+        return 1;
     };
     int isComplete(lua_State *L) {
         if( lock() ){
@@ -87,9 +116,12 @@ public:
     };
 private:
     lua_State* luaVM;
-    bool complete;
+    lua_State* mainVM;
+    bool complete, hasCallback;
+    int callbackFunc, numReturns;
     std::string execute;
     std::string returnVal;
+    ofMutex* mutex;
 };
 
 
@@ -98,5 +130,6 @@ const char bludAsycCurl::className[] = "bludAsycCurl";
 Lunar<bludAsycCurl>::RegType bludAsycCurl::methods[] = {
 	method(bludAsycCurl, process),
 	method(bludAsycCurl, isComplete),
+    method(bludAsycCurl, getReturnValue),
 	{0,0}
 };
